851e82950f336d1723e54f03c5f7c14ec54bf8d6
jbecerraromo <becerraromo@gmail.com>
2023-01-31 19:26:41 +0100

    Ejercicios hasta el 7
    Este commit incluye todos los ejercicios hasta el número 7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
 
+const repeatString = function getRepeatString(nom = string,number) {
+if(number < 0) return 'ERROR'
+
+ string = nom.repeat(number)
+
+ return string
 };
+
+const number = Math.floor(Math.random() * 10)
+for(i = 0; i < number; i++){
+    
+    repeatString('hey',number)
+}
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+
+  let toCelsius = (number - 32) * 5/9
+  let roundedCelsius = Math.round(toCelsius * 10) /10
+
+  return roundedCelsius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+
+  let toFahrenheit= (number * 9/5 +32)
+  let roundedFahrenheit = Math.round(toFahrenheit * 10 ) /10
+  return roundedFahrenheit
 };
 
 // Do not edit below this line
